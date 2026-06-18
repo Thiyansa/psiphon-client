f3ea9ab792acacb6ce54e888a7eb96ca52f2cc94
Thiyansa Ravidu <93363542+Thiyansa@users.noreply.github.com>
2026-06-18 10:01:23 +0530

    Update Psiphon client download URL

--- psiphon-client.sh
+++ psiphon-client.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo wget -4 -q -O /usr/local/bin/psiphon-client https://raw.githubusercontent.com/ndevxlk/psiphon-client/main/psiphon-client
+sudo wget -4 -q -O /usr/local/bin/psiphon-client https://raw.githubusercontent.com/Thiyansa/psiphon-client/main/psiphon-client
 sudo chmod +x /usr/local/bin/psiphon-client
 psiphon-client
